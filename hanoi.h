30387be850ad7896bcf6d4fd0a689fe6e950792d
Gustavo Brito <gustavobrito2416@gmail.com>
2025-06-15 17:16:03 -0300

    refactor/feat(DesenharJogo):Criei uma função que vai imprimir as funcionalidade, e dei uma refatora na main

--- hanoi.h
+++ hanoi.h
@@ -1,15 +1,16 @@
-#include HANOI_H
-#define PILHA_H
+#ifndef HANOI_H
+#define HANOI_H
 
-typedef strutc Disco;
+typedef struct Disco {
+    int tamanho;
+    struct Disco *prox;
+} Disco;
 
-void criarDsico(Disco* disco);
+void criarDisco(Disco* disco);
 void destruir(Disco * disco);
-int desempilhar(Disco **pilha)
-void empilhar(Disco **pilha, int tamanho)
-void showDisco();
-void push(Disco *disco);
-void pop();
-int stackIsEmpty();
+int desempilhar(Disco **pilha);
+void empilhar(Disco **pilha, int tamanho);
+void showPilha(Disco *pilha);
+int stackIsEmpty(Disco *pilha);
 
 #endif
